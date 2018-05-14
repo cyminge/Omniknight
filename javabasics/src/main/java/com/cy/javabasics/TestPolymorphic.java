aa3219d6b25bc6a47c24472c527737890cd71690
zhanmin <zhanmin@gionee.com>
2018-05-14 16:33:58 +0800

    73740 添加下载module

---
+++ javabasics/src/main/java/com/cy/javabasics/TestPolymorphic.java
@@ -0,0 +1,34 @@
+package com.cy.javabasics;
+
+/**
+ * 多态测试
+ */
+public class TestPolymorphic {   //例子3
+
+    public static void main(String[] args) {
+        Person p = new Man();
+        System.out.println(p.type);        //返回结果为P
+        System.out.println(p.getName());   //返回结果为Person
+    }
+
+}
+
+class Person {
+
+    String type = "P";
+
+    public static String getName() {
+        String name = "Person";
+        return name;
+    }
+}
+
+class Man extends Person {
+
+    String type = "M";
+
+    public static String getName() {
+        String name = "Man";
+        return name;
+    }
+}
---
+++ javabasics/src/main/java/com/cy/javabasics/myClass.java
@@ -0,0 +1,4 @@
+package com.cy.javabasics;
+
+public class myClass {
+}
